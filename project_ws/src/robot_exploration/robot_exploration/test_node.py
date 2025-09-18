94ea7da22160c8c77e50d4d7482db7ffba228f53
anismenouar20 <anis.menouar@polymtl.ca>
2025-09-18 16:22:19 +0000

    feat(launch):changed the name of the topic in the launch file

--- project_ws/src/robot_exploration/robot_exploration/test_node.py
+++ project_ws/src/robot_exploration/robot_exploration/test_node.py
@@ -6,7 +6,7 @@ from geometry_msgs.msg import Twist
 class CmdVelPublisher(Node):
     def __init__(self):
         super().__init__('cmd_vel_publisher')
-        self.publisher_ = self.create_publisher(Twist, '/cmd_vel', 10)
+        self.publisher_ = self.create_publisher(Twist, '/limo/cmd_vel', 10)
         self.timer = self.create_timer(0.5, self.timer_callback)
 
     def timer_callback(self):
@@ -16,8 +16,11 @@ class CmdVelPublisher(Node):
         msg.angular.z = 0.5
         #msg.linear.x = 0.2
         #msg.angular.z = 0.1
+
         self.publisher_.publish(msg)
         self.get_logger().info(f'Publishing: linear.x={msg.linear.x}, angular.z={msg.angular.z}')
+
+
 
 def main(args=None):
     rclpy.init(args=args)
