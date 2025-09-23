195352a56da2381e9513875d041e004760e3964d
Matis Roux <matis.roux@polymtl.ca>
2025-09-23 14:27:25 -0400

    refactor: remove unused components

--- client/src/main.ts
+++ client/src/main.ts
@@ -4,9 +4,7 @@ import { bootstrapApplication } from '@angular/platform-browser';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { Routes, provideRouter, withHashLocation } from '@angular/router';
 import { AppComponent } from '@app/pages/app/app.component';
-import { GamePageComponent } from '@app/pages/game-page/game-page.component';
 import { MainPageComponent } from '@app/pages/main-page/main-page.component';
-import { MaterialPageComponent } from '@app/pages/material-page/material-page.component';
 import { environment } from './environments/environment';
 
 if (environment.production) {
@@ -17,8 +15,6 @@ if (environment.production) {
 const routes: Routes = [
     { path: '', redirectTo: '/home', pathMatch: 'full' },
     { path: 'home', component: MainPageComponent },
-    { path: 'game', component: GamePageComponent },
-    { path: 'material', component: MaterialPageComponent },
     { path: '**', redirectTo: '/home' },
 ];
 
