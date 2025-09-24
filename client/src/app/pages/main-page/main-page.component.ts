e912481acea337bed3f8dba35a82099b410d3ac0
patrick <panzu@polymtl.ca>
2025-09-24 14:27:22 -0400

    feat(merge): fixed merge issues and cleaned server

--- client/src/app/pages/main-page/main-page.component.ts
+++ client/src/app/pages/main-page/main-page.component.ts
@@ -1,8 +1,4 @@
-// import { HttpErrorResponse } from '@angular/common/http';
 import { Component } from '@angular/core';
-// import { CommunicationService } from '@app/services/communication.service';
-// import { Message } from '@common/message';
-// import { BehaviorSubject } from 'rxjs';
 import { RobotLoginComponent } from '@app/components/robot-login/robot-login.component';
 
 
@@ -13,44 +9,4 @@ import { RobotLoginComponent } from '@app/components/robot-login/robot-login.com
     imports: [RobotLoginComponent],
 })
 export class MainPageComponent {
-
-    // sendTimeToServer(): void {
-    //     const newTimeMessage: Message = {
-    //         title: 'Hello from the client',
-    //         body: 'Time is : ' + new Date().toString(),
-    //     };
-    //     // Important de ne pas oublier "subscribe" ou l'appel ne sera jamais lancé puisque personne l'observe
-    //     this.communicationService.basicPost(newTimeMessage).subscribe({
-    //         next: (response) => {
-    //             const responseString = `Le serveur a reçu la requête a retourné un code ${response.status} : ${response.statusText}`;
-    //             this.message.next(responseString);
-    //         },
-    //         error: (err: HttpErrorResponse) => {
-    //             const responseString = `Le serveur ne répond pas et a retourné : ${err.message}`;
-    //             this.message.next(responseString);
-    //         },
-    //     });
-    // }
-
-    // getMessagesFromServer(): void {
-    //     this.communicationService
-    //         .basicGet()
-    //         // Cette étape transforme l'objet Message en un seul string
-    //         .pipe(
-    //             map((message: Message) => {
-    //                 return `${message.title} ${message.body}`;
-    //             }),
-    //         )
-    //         .subscribe(this.message);
-    // }
-    // onIdentify(): void {
-    //     this.identifyService.identifyRobot().subscribe({
-    //         next: (res: any) => {
-    //             this.message.next(`Réponse du robot : ${res.message}`);
-    //         },
-    //         error: (err: HttpErrorResponse) => {
-    //             this.message.next(`Erreur lors de l’identification : ${err.message}`);
-    //         },
-    //     });
-    // }
 }
