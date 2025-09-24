a27d219fd52de31f1fbd1f983296f4e7519a1e93
patrick <panzu@polymtl.ca>
2025-09-24 15:02:50 -0400

    Revert "feat(merge): fixed merge issues and cleaned server"
    This reverts commit e912481acea337bed3f8dba35a82099b410d3ac0.

--- server/app/app.module.ts
+++ server/app/app.module.ts
@@ -1,14 +1,30 @@
 import { Logger, Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { Course, courseSchema } from '@app/model/database/course';
+import { CourseController } from '@app/controllers/course/course.controller';
+import { CourseService } from '@app/services/course/course.service';
+import { DateController } from '@app/controllers/date/date.controller';
 import { DateService } from '@app/services/date/date.service';
+import { ChatGateway } from '@app/gateways/chat/chat.gateway';
+import { ExampleService } from '@app/services/example/example.service';
+import { ExampleController } from '@app/controllers/example/example.controller';
 import { RosService } from '@app/services/ros.service';
 import { RosController } from '@app/controllers/ros.controller';
 
 @Module({
     imports: [
         ConfigModule.forRoot({ isGlobal: true })
+        // MongooseModule.forRootAsync({
+        //     imports: [ConfigModule],
+        //     inject: [ConfigService],
+        //     useFactory: async (config: ConfigService) => ({
+        //         uri: config.get<string>('DATABASE_CONNECTION_STRING'), // Loaded from .env
+        //     }),
+        // }),
+        // MongooseModule.forFeature([{ name: Course.name, schema: courseSchema }]),
     ],
-    controllers: [RosController],
-    providers: [DateService, Logger, RosService],
+    controllers: [DateController, ExampleController, RosController], // CourseController, 
+    providers: [ChatGateway, DateService, ExampleService, Logger, RosService], // CourseService,
 })
 export class AppModule {}
