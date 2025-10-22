4cb4ddc2bf91db3ed57aca08b09b31b9f2be18af
Matis Roux <matis.roux@polymtl.ca>
2025-10-22 10:19:12 -0400

    fix: CMake error in server container fixed

--- docker/entrypoint.robot.sh
+++ docker/entrypoint.robot.sh
@@ -4,7 +4,7 @@ set -eo pipefail
 source /opt/ros/"${ROS_DISTRO}"/setup.bash
 cd /ws
 
-colcon build --symlink-install
+# colcon build --symlink-install
 
 colcon build --merge-install --symlink-install
 source install/setup.bash
