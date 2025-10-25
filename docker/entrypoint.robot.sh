016ebee0507ae071fa9fa353e1318d054d444fca
Matis Roux <matis.roux@polymtl.ca>
2025-10-25 16:40:08 -0400

    style: clean up of the code

--- docker/entrypoint.robot.sh
+++ docker/entrypoint.robot.sh
@@ -4,8 +4,6 @@ set -eo pipefail
 source /opt/ros/"${ROS_DISTRO}"/setup.bash
 cd /ws
 
-# colcon build --symlink-install
-
 colcon build --merge-install --symlink-install
 source install/setup.bash
 
