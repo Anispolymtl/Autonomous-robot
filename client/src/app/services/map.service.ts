8f351b4db80a6edcc44a75118bcf07933904de1e
patrick <panzu@polymtl.ca>
2025-11-04 22:02:38 -0500

    feat(map): WIP

--- client/src/app/services/map.service.ts
+++ client/src/app/services/map.service.ts
@@ -27,10 +27,38 @@ export class MapService {
             this.map = {data: recoveredMap.data, height: recoveredMap.info.height, width: recoveredMap.info.width}
             console.log(recoveredMap);
             console.log(this.map);
+            this.renderMap();
         });
     }
 
     resetMap() {
         this.map = undefined;
+    }
+
+    renderMap(): void {
+        if (!this.map) return;
+
+        const { data, width, height } = this.map;
+        const int8View = new Int8Array(data);
+
+        const canvas = document.getElementById('mapCanvas') as HTMLCanvasElement;
+        const ctx = canvas.getContext('2d')!;
+        const imageData = ctx.createImageData(width, height);
+
+        for (let i = 0; i < int8View.length; i++) {
+            const val = int8View[i];
+            let color: number;
+
+            if (val === -1) color = 127;
+            else color = 255 - (val * 2.55);
+
+            imageData.data[i * 4 + 0] = color;
+            imageData.data[i * 4 + 1] = color;
+            imageData.data[i * 4 + 2] = color;
+            imageData.data[i * 4 + 3] = 255;
+        }
+
+        ctx.putImageData(imageData, 0, 0);
+        ctx.scale(1, -1);
     }
 }
